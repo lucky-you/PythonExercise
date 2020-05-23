6c2f756a4c767c38be097685bb1c85ab6ce843de
1021237228@qq.com <zhou11060708>
2020-05-23 22:59:28 +0800

    爬取单张图片并保存到本地

--- paChong/TextOne.py
+++ paChong/TextOne.py
@@ -1,10 +1,38 @@
-
 # 爬虫的练习使用
 
-import numpy
 import requests
+import os
 
 
+def getHTMLText(url):
+    try:
+        r = requests.get(url, timeout=30)
+        r.raise_for_status()
+        r.encoding = r.apparent_encoding
+        return r.text
+    except:
+        return "产生了异常"
 
 
+def downLoadImage(url):
+    root = "D://pics//"
+    path = root + url.split("-")[1] + ".jpg"
+    try:
+        if not os.path.exists(root):
+            os.makedirs(root)
+        if not os.path.exists(path):
+            r = requests.get(url)
+            r.raise_for_status()
+            with open(path, "wb") as f:
+                f.write(r.content)
+                f.close()
+                print("文件保存成功")
+        else:
+            print("文件已存在")
+    except:
+        return "爬取错误了"
 
+
+if __name__ == '__main__':
+    url = "http://hbimg.b0.upaiyun.com/bc18f16be6a599d2649b645ece7b7300d281853b87f93-TxNHuG_fw658"
+    downLoadImage(url)
